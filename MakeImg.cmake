d8f0c36db477cf806fe5bd802cb7ae845cc6d2e9
Michal Schulz <michal.schulz@gmx.de>
2025-02-23 08:54:27 +0000

    img generation works

--- MakeImg.cmake
+++ MakeImg.cmake
@@ -40,58 +40,45 @@ set(FAT_IMAGE ${CD_FOLDER}/image.fat)
 #    #COMMAND xorrisofs -o ${GPT_IMAGE} -R -J -v -d -N -hide-rr-moved -no-emul-boot -eltorito-platform efi -eltorito-boot image.fat ${CD_FOLDER}
 #)
 
+message("-- Creating and partition boot image")
 execute_process(
     COMMAND dd if=/dev/zero of=${GPT_IMAGE} bs=1M count=3
-#    OUTPUT_QUIET
-#    ERROR_QUIET
+    OUTPUT_QUIET
+    ERROR_QUIET
 )
 
 execute_process(
     COMMAND parted -a none ${GPT_IMAGE} mklabel gpt
-#    OUTPUT_QUIET
-#    ERROR_QUIET
+    OUTPUT_QUIET
+    ERROR_QUIET
 )
 
 execute_process(
     COMMAND parted -a none ${GPT_IMAGE} mkpart ESP fat32 32KiB 2MiB
-#    OUTPUT_QUIET
-#    ERROR_QUIET
+    OUTPUT_QUIET
+    ERROR_QUIET
 )
 
 execute_process(
     COMMAND parted -a none ${GPT_IMAGE} set 1 boot on
-#    OUTPUT_QUIET
-#    ERROR_QUIET
+    OUTPUT_QUIET
+    ERROR_QUIET
 )
+
+message("-- Creating file system")
 
 execute_process(
-#    COMMAND mkdir ${CD_FOLDER}
-#    COMMAND dd if=/dev/zero of=${FAT_IMAGE} bs=1M count=2
     COMMAND mkfs.vfat -F 12  ${GPT_IMAGE} --offset 64 2048
-#    OUTPUT_QUIET
-#    ERROR_QUIET
+    OUTPUT_QUIET
+    ERROR_QUIET
 )
 
-#execute_process(
-#    COMMAND mmd -i ${GPT_IMAGE} -o 32768 ::EFI
-#    COMMAND mmd -i ${GPT_IMAGE} -o 32768 ::EFI/BOOT
-#    COMMAND mcopy -i ${GPT_IMAGE} -o 32768 -s ${CMAKE_BINARY_DIR}/BOOTaa64.EFI ::EFI/BOOT/BOOTaa64.EFI
-#    OUTPUT_QUIET
-#    ERROR_QUIET
-#)
+message("-- Copying boot files")
 
-#execute_process(
-#    COMMAND sgdisk -g -a 1 -n 1:48:+32M -c 1:BOOT\ Partition -t 1:ef00 ${GPT_IMAGE}
-#    OUTPUT_QUIET
-#    ERROR_QUIET
-#)
-
-#execute_process(
-#    COMMAND dd if=${FAT_IMAGE} of=${GPT_IMAGE} bs=1k seek=24 conv=notrunc
-#    OUTPUT_QUIET
-#    ERROR_QUIET
-#)
-
-#file(COPY
-#    ${GPT_IMAGE} DESTINATION ${CMAKE_INSTALL_PREFIX}/efi/
-#)
+execute_process(
+    COMMAND mmd -i ${GPT_IMAGE}@@32K ::EFI
+    COMMAND mmd -i ${GPT_IMAGE}@@32K ::EFI/BOOT
+    COMMAND mcopy -i ${GPT_IMAGE}@@32K ${CMAKE_BINARY_DIR}/BOOTaa64.EFI ::EFI/BOOT/BOOTaa64.EFI
+    OUTPUT_QUIET
+    ERROR_QUIET
+)
